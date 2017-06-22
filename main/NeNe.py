60d24b2efe16ce362e2fc834ca4c8393a41723ea
jmueller95 <julian.mueller@student.uni-tuebingen.de>
2017-06-22 16:58:13 +0200

    hab die numpy Arrays in der NeNe-Klasse konstruiert, kann sich Lisa jetz draufstürzen

--- main/NeNe.py
+++ main/NeNe.py
@@ -14,10 +14,33 @@ isBinderList = []
 with open(args.input, "r") as i:
     for e in i:
         lineSplit = e.split()
-        if lineSplit[0] == "Peptide": #skip the first line
+        if lineSplit[0] == "Peptide":  # skip the first line
             pass
         else:
             sequencesList.append(lineSplit[0])
-            isBinderList.append(lineSplit[1] is 1)
+            isBinderList.append(lineSplit[2] is "1")
 
-#Create numpy arrays of the sequence properties
+# Create a 2D-array of sequences, residue by residue
+residues = np.array([[residue for residue in sequence] for sequence in sequencesList])
+# residues' shape is 726*9
+
+# Create 2D arrays for each of the properties (not sure if we're actually gonna use this, 'residue' might be enough)
+# Weight
+weights = np.array(
+    [[aminoAcidDict.get(residue).weight for residue in sequence] for sequence in sequencesList])
+
+# IEP
+ieps = np.array(
+    [[aminoAcidDict.get(residue).iep for residue in sequence] for sequence in sequencesList])
+
+# Hydrophobicity
+hydrophobicities = np.array(
+    [[aminoAcidDict.get(residue).hydrophobicity for residue in sequence] for sequence in sequencesList])
+
+# Polarity
+polarities = np.array(
+    [[aminoAcidDict.get(residue).polarity for residue in sequence] for sequence in sequencesList])
+
+# Accessible Area
+areas = np.array(
+    [[aminoAcidDict.get(residue).area for residue in sequence] for sequence in sequencesList])
